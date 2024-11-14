fec658817e70c3fee0c78ec09c2c583484b9369a
KM4HS <ac4hsu@gmail.com>
2024-11-14 19:22:43 +0900

    INIT: init project

---
+++ src/main/java/com/example/todoappdevelop/TodoAppDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoappdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAppDevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todoappdevelop/TodoAppDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoappdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
